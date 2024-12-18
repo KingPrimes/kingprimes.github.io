5397d6fe035cb9df60c6186dc8f150d93025cbf6
KingPrimes <15066650082@163.com>
2024-12-18 22:48:46 +0800

    优化Live2d图片，并添加点击随机切换表情

--- static/js/live2d/om_live2d.js
+++ static/js/live2d/om_live2d.js
@@ -1,45 +1,46 @@
+let oml2d;
 const PLUGINS_LIVE2D = new (function () {
-    OML2D.loadOml2d({
+    oml2d = OML2D.loadOml2d({
         // 插件所在的位置
         dockedPosition: 'right',
         // 提示词
-        tips:{
+        tips: {
             // 复制时
             copyTips: {
                 message: ['复制了啥?'],
             },
-            idleTips:{
-                message:['Live2D模型来自B站 @星临之海'],
-                wordTheDay(wd){
+            idleTips: {
+                message: ['Live2D模型来自B站 @星临之海'],
+                wordTheDay(wd) {
                     return `${wd.hitokoto}  by.${wd.from}`
                 },
             },
-            welcomeTips:{
-                message:{
+            welcomeTips: {
+                message: {
                     'default': ['欢迎光临~']
                 },
             }
         },
         // 菜单选项
-        menus:{
+        menus: {
             // 禁用菜单
             disable: true
         },
         // 模型列表
-        models:[
+        models: [
             {
                 // 模型名称
                 name: 'moon_rabbit',
                 path: '/data/live2d/moon_rabbit/psd_b.model3.json',
-                position: [0,60],
+                position: [0, 60],
                 scale: 0.08,
                 stageStyle: {
                     height: 450
-                },
-                motionPreloadStrategy: 'all'
+                }
             }
         ]
-    })
+    });
+
 })
 (() => {
     // It will be executed when the DOM tree is built.
