623ea4dbc46d3b11b0179e54d6784078cfd30718
KingPrimes <Sakitama_q@163.com>
2025-01-27 21:56:36 +0800

    refactor(live2d): 更新 Live2D 模型配置和数据 - 添加了新的 Live2D模型 - 修正了模型来源，点击模型关于直接跳转来源地址

--- static/js/live2d/om_live2d.js
+++ static/js/live2d/om_live2d.js
@@ -10,7 +10,6 @@ const PLUGINS_LIVE2D = new (function () {
                 message: ['复制了啥?'],
             },
             idleTips: {
-                message: ['Live2D模型来自B站 @星临之海'],
                 wordTheDay(wd) {
                     return `${wd.hitokoto}  by.${wd.from}`
                 },
@@ -20,11 +19,6 @@ const PLUGINS_LIVE2D = new (function () {
                     'default': ['欢迎光临~']
                 },
             }
-        },
-        // 菜单选项
-        menus: {
-            // 禁用菜单
-            disable: true
         },
         // 模型列表
         models: [
@@ -37,10 +31,68 @@ const PLUGINS_LIVE2D = new (function () {
                 stageStyle: {
                     height: 450
                 }
+            },
+            {
+                name: 'maolili_vts',
+                path: '/data/live2d/maolili_vts/mailili.model3.json',
+                position: [0, 60],
+                scale: 0.08,
+                stageStyle: {
+                    height: 450
+                }
             }
-        ]
-    });
+        ],
+        // 菜单选项
+        menus: {
+            // 禁用菜单
+            disable: false,
+            items: [
+                {
+                    id: "Rest",
+                    icon: "icon-rest",
+                    title: "休息",
+                    onClick(i) {
+                        let t;
+                        i.statusBarOpen((t = i.options.statusBar) == null ? void 0 : t.restMessage)
+                        i.clearTips()
+                        i.setStatusBarClickEvent(() => {
+                                i.statusBarClose()
+                                i.stageSlideIn()
+                                i.statusBarClearEvents()
+                            }
+                        )
+                        i.stageSlideOut()
+                    }
+                }, {
+                    id: "SwitchModel",
+                    icon: "icon-switch",
+                    title: "切换模型",
+                    onClick(i) {
+                        i.loadNextModel()
+                    }
+                }, {
+                    id: "About",
+                    icon: "icon-about",
+                    title: "关于模型",
+                    onClick(i) {
+                        switch (i.models.currentModelIndex){
+                            case 0:
+                                window.open('https://www.bilibili.com/video/BV1cR4y1Y7vw')
+                                break;
+                            case 1:
+                                window.open('https://www.bilibili.com/video/BV1sd6gY2EY9')
+                                break;
+                        }
+                    }
+                }
+            ]
+        },
 
+    });
+    this.initPluginsLive2d = function () {
+        // 初始化
+        oml2d.init();
+    }
 })
 (() => {
     // It will be executed when the DOM tree is built.
